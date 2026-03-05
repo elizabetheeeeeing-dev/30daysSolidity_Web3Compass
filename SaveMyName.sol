f3d05aa66ff7686e3b333739ee06bef85e30d19e
周星星（Elizabeth Chou） <elizabetheeeeeing@gmail.com>
2026-03-05 13:48:31 +0800

    Enhance SaveMyName contract with age and job fields

--- SaveMyName.sol
+++ SaveMyName.sol
@@ -6,14 +6,28 @@ contract SaveMyName{
      
   string name;
   string bio;
+  uint256 age;
+  string job;
 
-  function add (string memory _name, string memory _bio )public {
+  function add(
+      string memory _name,
+      string memory _bio,
+      uint256 _age,
+      string memory _job
+  ) public {
     name = _name;
     bio = _bio;
+    age = _age;
+    job = _job;
   }
 
-  function retrieve() public view returns(string memory, string memory){
-    return (name,bio);
+  function retrieve() public view returns(
+      string memory,
+      string memory,
+      uint256,
+      string memory
+  ){
+    return (name, bio, age, job);
   }
 
 }
